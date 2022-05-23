0baf9d832fb73130baaa5d68a7eb4f1344bd35fe
gsnyder206 <gsnyder@stsci.edu>
2022-05-23 12:16:26 -0400

    initial work

--- setup_foggie_pd_run.py
+++ setup_foggie_pd_run.py
@@ -3,17 +3,20 @@ import glob
 import numpy as np
 import sys
 
-def setup_pd_snapshot(input_file,
+def setup_pd_snapshot(input_snap,
                     output_dir='/nobackup17/gfsnyder/foggie-pd-outputs/',
                     pd_master_template='/home/gfsnyder/PythonCode/pdmocks/parameters_master.py',
                     pd_model_template='/home/gfsnyder/PythonCode/pdmocks/parameters_model.py'):
 
     #identify input snapshot
-    print('setting up: ', input_file)
+    print('setting up: ', input_snap)
 
     #create output directory
+    snapname = os.path.basename(input_snap)
+    runname = os.path.basename(os.path.dirname(input_snap))
+    haloname = os.path.basename(os.path.dirname(os.path.dirname(input_snap)))
 
-
+    print(snapname, runname, haloname)
     #copy parameter files to output directory
 
 
