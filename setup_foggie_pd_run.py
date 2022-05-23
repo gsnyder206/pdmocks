ccee66d791adbfb4f9b7ef38cdc5a555820361de
gsnyder206 <gsnyder@stsci.edu>
2022-05-23 12:09:00 -0400

    initial work

--- setup_foggie_pd_run.py
+++ setup_foggie_pd_run.py
@@ -8,5 +8,21 @@ def setup_pd_snapshot(input_file,
                     pd_master_template='/home/gfsnyder/PythonCode/pdmocks/parameters_master.py',
                     pd_model_template='/home/gfsnyder/PythonCode/pdmocks/parameters_model.py'):
 
+    #identify input snapshot
+    print('setting up: ', input_file)
+
+    #create output directory
+
+
+    #copy parameter files to output directory
+
+
+    #edit parameter files
+
+
+    #create runscript
+
+
+
 
 return
